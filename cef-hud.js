bfb992d4869fcdf02157bdfc1baff238bb06a25a
Royzen2 <64889727+Royzen2@users.noreply.github.com>
2021-09-25 11:07:18 +0300

    fix weapon

--- cef-hud.js
+++ cef-hud.js
@@ -1,7 +1,9 @@
 
 //royzen ( vk.com/weckek )
 
+
 cef.emit('hud:start');
+
 //нужен плагин CEF-INTERFACE.dll
 //скачать можно на форуме, чтобы он заработал в папке cef создать plugins и туда закинуть.
 
@@ -26,6 +28,7 @@ function getPolice(police)
 {
         for(let i = 0; i < police; i++)
         {
+                
                 document.getElementById('wanted_' + i).style.fill = '#ffa200';
         }
 
@@ -35,49 +38,142 @@ function getWeaponImage(weaponid) {
         case 1:
         return '1';
         break;
-        case 3:
+        case 2:
         return '2';
         break;
-        case 4:
+        case 3:
         return '3';
         break;
-        case 5:
+        case 4:
         return '4';
         break;
-        case 9:
+        case 5:
         return '5';
         break;
+        case 6:
+        return '6';
+        break;
+        case 7:
+        return '7';
+        break;
+        case 8:
+        return '8';
+        break;
+        case 9:
+        return '9';
+        break;
+        case 10:
+        return '10';
+        break;
+        case 11:
+        return '11';
+        break;
+        case 12:
+        return '12';
+        break;
+        case 13:
+        return '13';
+        break;
+        case 14:
+        return '14';
+        break;
+        case 15:
+        return '15';
+        break;
+        case 16:
+        return '16';
+        break;
+        case 17:
+        return '17';
+        break;
+        case 18:
+        return '18';
+        break;
+        case 19:
+        return '19';
+        break;
+        /*case 20:
+        return '20';
+        break;
+        case 21:
+        return '21';*/
+        break;
+        case 22:
+        return '22';
+        break;
         case 23:
-        return 'silenced';
+        return '23';
         break;
         case 24:
-        return 'makarov';
+        return '24';
+        break;
+        case 25:
+        return '25';
+        break;
+        case 26:
+        return '26';
+        break;
+        case 27:
+        return '27';
+        break;
+        case 28:
+        return '28';
         break;
         case 29:
-        return 'aksu';
+        return '29';
         break;
         case 30:
-        return 'ak';
+        return '30';
         break;
         case 31:
-        return 'akm';
+        return '31';
         break;
+        case 32:
+        return '32';
+        break;
+        case 33:
+        return '33';
+        break;       
         case 34:
-        return 'svd';
-        break;
+        return '34';
+        break;       
+        case 35:
+        return '35';
+        break;       
+        case 36:
+        return '36';
+        break;       
+        case 37:
+        return '37';
+        break;       
+        case 38:
+        return '38';
+        break;       
+        case 39:
+        return '39';
+        break;       
+        case 40:
+        return '40';
+        break;       
         case 41:
-        return 'spr';
-        break;
+        return '41';
+        break;       
         case 42:
-        return 'fire';
-        break;
+        return '42';
+        break;       
         case 43:
-        return 'cam';
-        break;
+        return '43';
+        break;       
+        case 44:
+        return '44';
+        break;       
+        case 45:
+        return '45';
+        break;       
         case 46:
-        return 'par';
-        break;
+        return '46';
+        break;       
         default:
-        return 'fist';
+        return '0';
     }
 }
